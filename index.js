c153592106a657251b394e4b45a9621fe48bd06d
Dylan Raye-Leonard <drayeleonard@gmail.com>
2022-04-06 14:42:18 -0600

    commit tree a6a130942dd113462e44e3aac4b843cda2e3bd6c

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+
+
+function writeCards(names, event) {
+    const cards = [];
+
+    for (let i = 0; i < names.length; i++) {
+        cards[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    }
+    return cards;
+}
+
+
+function countDown(int) {
+    while (int >= 0) {
+        console.log(int);
+        int --;
+    }
+}
